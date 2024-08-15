de3e06edaaff9d9104c3ed8012d120db5819aa23
V-Gulev <velizargulev@abv.bg>
2024-08-15 16:50:27 +0300

    Equal Sums

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,41 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] array = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        topInts(array);
-    }
 
-    public static void topInts(int[] array) {
-        int n = array.length;
-        List<Integer> topIntegers = new ArrayList<>();
-        int maxFromRight = array[n - 1];
-        topIntegers.add(maxFromRight);
-        for (int i = n - 2; i >= 0; i--) {
-            if (array[i] > maxFromRight) {
-                maxFromRight = array[i];
-                topIntegers.add(maxFromRight);
+        String numbers = scanner.nextLine();
+        String[] symbols = numbers.split(" ");
+        int[] array = new int[symbols.length];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(symbols[i]);
+        }
+
+        int totalSum = 0;
+        for (int j : array) {
+            totalSum += j;
+        }
+
+        int leftsum = 0;
+        int rightSum;
+        boolean isThereAnIndex = true;
+
+        if (array.length == 1 || array.length == 0) {
+            System.out.println(0);
+            isThereAnIndex= false;
+        } else {
+            for (int i = 0; i < array.length - 1; i++) {
+                rightSum = totalSum - array[i] - leftsum;
+                leftsum = totalSum - array[i] - rightSum;
+                if (leftsum == rightSum) {
+                    System.out.println(i);
+                    isThereAnIndex = false;
+                    break;
+                }
+                leftsum += array[i];
             }
         }
-        Collections.reverse(topIntegers);
-        for (int top : topIntegers) {
-            System.out.print(top + " ");
+        if (isThereAnIndex){
+            System.out.println("no");
         }
     }
 }
