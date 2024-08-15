6ca0033d3c08895eb4958911a22fb5f7a2265f49
V-Gulev <velizargulev@abv.bg>
2024-08-15 16:39:43 +0300

    Reverse Array of Strings

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,23 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] numbers = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int sum =0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                sum += number;
-            }
+        String input = scanner.nextLine();
+        String[] chars = input.split(" ");
+        reversed(chars);
+        for (String aChar : chars) {
+            System.out.print(aChar);
+            System.out.print(" ");
         }
-        System.out.println(sum);
+    }
 
+    private static void reversed(String[] array) {
+        int n = array.length;
+        for (int i = 0; i < n / 2; i++) {
+            String a = array[i];
+            array[i] = array[n - i - 1];
+            array[n - i - 1] = a;
+        }
     }
 }
-
