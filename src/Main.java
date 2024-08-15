1cbf01c9c1c64d5cc12cb7ae31795093357e0a6e
V-Gulev <velizargulev@abv.bg>
2024-08-15 16:43:41 +0300

    Train

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,20 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] firstArray = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int[] secondArray = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        boolean check = false;
-        int sum1;
-        int sum2;
-        int totalSum = 0;
-        int maxLength = Math.max(firstArray.length, secondArray.length);
-        for (int i = 0; i < maxLength; i++) {
-            sum1 = firstArray[i];
-            sum2 = secondArray[i];
-            totalSum += firstArray[i];
-            if (sum1 != sum2) {
-                System.out.printf("Arrays are not identical. Found difference at %d index.", i);
-                check = false;
-                break;
-            }else check=true;
+        int wagons = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+        int[] people = new int[wagons];
+        for (int i = 0; i < wagons; i++) {
+            people[i] = scanner.nextInt();
+            sum += people[i];
         }
-        if (check) {
-            System.out.printf("Arrays are identical. Sum: %d", totalSum);
+        for (int person : people) {
+            System.out.print(person+" ");
         }
-
+        System.out.println();
+        System.out.println(sum);
     }
 }
 
