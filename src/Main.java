764668400076010e045e31e3a6803755d093def1
V-Gulev <velizargulev@abv.bg>
2024-08-15 16:42:22 +0300

    Equal Arrays

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] numbers = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int sumEven = 0;
-        int sumOdd = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                sumEven += number;
-            } else {
-                sumOdd += number;
-            }
+        int[] firstArray = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] secondArray = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        boolean check = false;
+        int sum1;
+        int sum2;
+        int totalSum = 0;
+        int maxLength = Math.max(firstArray.length, secondArray.length);
+        for (int i = 0; i < maxLength; i++) {
+            sum1 = firstArray[i];
+            sum2 = secondArray[i];
+            totalSum += firstArray[i];
+            if (sum1 != sum2) {
+                System.out.printf("Arrays are not identical. Found difference at %d index.", i);
+                check = false;
+                break;
+            }else check=true;
         }
-        System.out.println(sumEven-sumOdd);
+        if (check) {
+            System.out.printf("Arrays are identical. Sum: %d", totalSum);
+        }
 
     }
 }
