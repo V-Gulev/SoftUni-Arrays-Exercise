99452d12d2d5ea9779ba486ef25aec8d4d17ef30
V-Gulev <velizargulev@abv.bg>
2024-08-15 16:44:12 +0300

    Common Elements

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,25 @@
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int wagons = Integer.parseInt(scanner.nextLine());
-        int sum = 0;
-        int[] people = new int[wagons];
-        for (int i = 0; i < wagons; i++) {
-            people[i] = scanner.nextInt();
-            sum += people[i];
+        String[] array1 = scanner.nextLine().split(" ");
+        String[] array2 = scanner.nextLine().split(" ");
+        common(array1, array2);
+    }
+
+    public static void common(String[] array1, String[] array2) {
+        Set<String> set1 = new HashSet<>();
+        Collections.addAll(set1, array1);
+
+        for (String element : array2) {
+            if (set1.contains(element)) {
+                System.out.print(element + " ");
+            }
         }
-        for (int person : people) {
-            System.out.print(person+" ");
-        }
-        System.out.println();
-        System.out.println(sum);
     }
 }
-
