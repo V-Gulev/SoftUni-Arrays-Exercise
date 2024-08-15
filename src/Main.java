8f5f4091910a51f7ffa3d94354ac4ac710b36a4b
V-Gulev <velizargulev@abv.bg>
2024-08-15 16:37:33 +0300

    Print Numbers in Reverse

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int day = Integer.parseInt(scanner.nextLine());
-        String[] names={"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-        if (day>=1 && day<=7){
-            System.out.println(names[day-1]);
-        }else System.out.println("Invalid day!");
+        int times = Integer.parseInt(scanner.nextLine());
+        int[] ar = new int[times];
+        for (int i = 0; i < times; i++) {
+            int number = Integer.parseInt(scanner.nextLine());
+            ar[i] = number;
+        }
+        for (int i = ar.length-1; i >=0 ; i--) {
+            System.out.print(ar[i]+" ");
+        }
     }
 }
 
