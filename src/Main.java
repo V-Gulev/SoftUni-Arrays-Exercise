4391105f11781cd4a293f9916ba44db2b09a1634
V-Gulev <velizargulev@abv.bg>
2024-08-15 16:45:32 +0300

    Array Rotation

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,20 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = Integer.parseInt(scanner.nextLine());
-        int[] red = new int[n];
-        int[] blue = new int[n];
-        for (int i = 0; i < n; i++) {
-            int[] array = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            if (i % 2 == 0) {
-                red[i] = array[0];
-                blue[i] = array[1];
-            } else {
-                red[i] = array[1];
-                blue[i] = array[0];
-            }
+        int[] firstArray = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int rotations = Integer.parseInt(scanner.nextLine());
+        firstArray = rotate(firstArray, rotations);
+        for (int j : firstArray) {
+            System.out.print(j + " ");
         }
+    }
 
-        for (int j : red) {
-            System.out.print(j+" ");
+    public static int[] rotate(int[] array, int rotations) {
+        int n = array.length;
+        int[] rotatedArray = new int[n];
+        for (int i = 0; i < n; i++) {
+            rotatedArray[i] = array[(i + rotations) % n];
         }
-        System.out.println();
-        for (int j : blue) {
-            System.out.print(j+" ");
-        }
+        return rotatedArray;
     }
 }
