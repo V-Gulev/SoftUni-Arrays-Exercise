8e3bf6851680a794a71861d181379e58712340c4
V-Gulev <velizargulev@abv.bg>
2024-08-16 14:50:23 +0300

    Longest Increasing Subsequence

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,46 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int number = Integer.parseInt(scanner.nextLine());
-        System.out.println(GetFibonacci(number));
-    }
-
-    public static int GetFibonacci(int number) {
-        if (number == 1 || number == 2) {
-            return 1;
+        String[] stringNumbers = scanner.nextLine().split(" ");
+        int[] array = new int[stringNumbers.length];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(stringNumbers[i]);
         }
-        return GetFibonacci(number - 1) + GetFibonacci(number - 2);
+        int maxLISLength = 0;
+        int index = 0;
+        if (array.length == 1) {
+            System.out.println(array.length);
+        } else {
+            int[] len = new int[array.length];
+            int[] prev = new int[array.length];
+
+            for (int i = 0; i < array.length; i++) {
+                len[i] = 1;
+                prev[i] = -1;
+
+                for (int j = 0; j < i; j++) {
+                    if (array[j] < array[i] && len[j] + 1 > len[i]) {
+                        len[i] = len[j] + 1;
+                        prev[i] = j;
+                    }
+                }
+                if (len[i] > maxLISLength) {
+                    maxLISLength = len[i];
+                    index = i;
+                }
+            }
+            int[] longestIncreasingSequence = new int[maxLISLength];
+            int resultIndex = maxLISLength - 1;
+            while (index != -1) {
+                longestIncreasingSequence[resultIndex] = array[index];
+                index = prev[index];
+                resultIndex--;
+            }
+            for (int j : longestIncreasingSequence) {
+                System.out.print(j + " ");
+            }
+        }
     }
 }
