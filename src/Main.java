be2d51e8d574fbb6adbf47594533a3da4ffaee1e
V-Gulev <velizargulev@abv.bg>
2024-08-16 14:47:13 +0300

    Encrypt, Sort and Print

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,28 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
-    public static int[] arrayFromInput(String s) {
-
-        String[] items = s.split(" ");
-        int[] numbers = new int[items.length];
-
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = Integer.parseInt(items[i]);
-        }
-        return numbers;
-    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int peopleWaitingToGetOn = Integer.parseInt(scanner.nextLine());
-        int[] numbers = arrayFromInput(scanner.nextLine());
-        int adder;
-
-        for (int i = 0; i < numbers.length; i++) {
-            if (peopleWaitingToGetOn >= 4) {
-                adder = 4 - numbers[i];
-                numbers[i] += adder;
-                peopleWaitingToGetOn -= adder;
-            } else if (peopleWaitingToGetOn > 0) {
-                adder = 4 - numbers[i];
-                if (adder>=peopleWaitingToGetOn){
-                    numbers[i] += peopleWaitingToGetOn;
-                    peopleWaitingToGetOn = 0;
-                }else {
-                    numbers[i] += adder;
-                    peopleWaitingToGetOn-=adder;
+        int number = Integer.parseInt(scanner.nextLine());
+        int[] allSums = new int[number];
+        for (int i = 0; i < number; i++) {
+            String name = scanner.nextLine();
+            int sum = 0;
+            for (int j = 0; j < name.length(); j++) {
+                char index = name.charAt(j);
+                if (index == 'A' || index == 'E' || index == 'I' || index == 'O' || index == 'U' || index == 'a' || index == 'e' || index == 'i' || index == 'o' || index == 'u') {
+                    sum += index * name.length();
+                } else {
+                    sum += index / name.length();
                 }
             }
+            allSums[i] = sum;
         }
-        if (peopleWaitingToGetOn > 0) {
-            System.out.printf("There isn't enough space! %d people in a queue!", peopleWaitingToGetOn);
-            System.out.println();
-            for (int j : numbers) {
-                System.out.print(j + " ");
-            }
-        } else if (numbers[numbers.length - 1] == 4) {
-            for (int j : numbers) {
-                System.out.print(j + " ");
-            }
-        } else {
-            System.out.println("The lift has empty spots!");
-            for (int j : numbers) {
-                System.out.print(j + " ");
-            }
+        Arrays.sort(allSums);
+        for (int k : allSums) {
+            System.out.println(k + " ");
         }
-
     }
 }
