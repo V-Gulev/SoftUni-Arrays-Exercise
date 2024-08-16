f9c6e8c6603cb461f1672a60d28ae391f3e14d29
V-Gulev <velizargulev@abv.bg>
2024-08-16 14:49:36 +0300

    Recursive Fibonacci

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int rows = Integer.parseInt(scanner.nextLine());
-        int[] currentRow = new int[rows];
-        int[] previousRow = new int[rows];
+        int number = Integer.parseInt(scanner.nextLine());
+        System.out.println(GetFibonacci(number));
+    }
 
-        for (int i = 0; i < rows; i++) {
-            currentRow[0] = 1;
-
-            for (int j = 1; j < i; j++) {
-                currentRow[j] = previousRow[j - 1] + previousRow[j];
-            }
-            currentRow[i] = 1;
-
-            for (int j = 0; j <= i; j++) {
-                System.out.print(currentRow[j] + " ");
-            }
-            System.out.println();
-            int[] swap = previousRow;
-            previousRow = currentRow;
-            currentRow = swap;
+    public static int GetFibonacci(int number) {
+        if (number == 1 || number == 2) {
+            return 1;
         }
-
-
+        return GetFibonacci(number - 1) + GetFibonacci(number - 2);
     }
 }
