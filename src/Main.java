81368f2151c8fc5a1af54a26b4240a2f825716aa
V-Gulev <velizargulev@abv.bg>
2024-08-16 14:48:23 +0300

    Pascal Triangle

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,30 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int number = Integer.parseInt(scanner.nextLine());
-        int[] allSums = new int[number];
-        for (int i = 0; i < number; i++) {
-            String name = scanner.nextLine();
-            int sum = 0;
-            for (int j = 0; j < name.length(); j++) {
-                char index = name.charAt(j);
-                if (index == 'A' || index == 'E' || index == 'I' || index == 'O' || index == 'U' || index == 'a' || index == 'e' || index == 'i' || index == 'o' || index == 'u') {
-                    sum += index * name.length();
-                } else {
-                    sum += index / name.length();
-                }
+        int rows = Integer.parseInt(scanner.nextLine());
+        int[] currentRow = new int[rows];
+        int[] previousRow = new int[rows];
+
+        for (int i = 0; i < rows; i++) {
+            currentRow[0] = 1;
+
+            for (int j = 1; j < i; j++) {
+                currentRow[j] = previousRow[j - 1] + previousRow[j];
             }
-            allSums[i] = sum;
+            currentRow[i] = 1;
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(currentRow[j] + " ");
+            }
+            System.out.println();
+            int[] swap = previousRow;
+            previousRow = currentRow;
+            currentRow = swap;
         }
-        Arrays.sort(allSums);
-        for (int k : allSums) {
-            System.out.println(k + " ");
-        }
+
+
     }
 }
