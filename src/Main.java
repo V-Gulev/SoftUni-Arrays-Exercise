9eb66ebfb6eb7141a343d2b8a589d4b35c21688a
V-Gulev <velizargulev@abv.bg>
2024-08-16 14:45:30 +0300

    Array Modifier

--- src/Main.java
+++ src/Main.java
@@ -15,19 +15,40 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] numbers = arrayFromInput(scanner.nextLine());
-        int finalValue = Integer.parseInt(scanner.nextLine());
 
-        int sum;
 
-        for (int i = 0; i < numbers.length - 1; i++) {
-            for (int j = i+1; j < numbers.length ; j++) {
-                sum = numbers[i] + numbers[j];
-                if (sum == finalValue) {
-                    System.out.println(numbers[i] + " " + numbers[j]);
-                }
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equals("end")) {
+                break;
+            }
+            String[] action = command.split(" ");
+
+            switch (action[0]) {
+                case "swap":
+                    int index1 = Integer.parseInt(action[1]);
+                    int index2 = Integer.parseInt(action[2]);
+                    int swapper = numbers[index1];
+                    numbers[index1] = numbers[index2];
+                    numbers[index2] = swapper;
+                    break;
+                case "multiply":
+                    index1 = Integer.parseInt(action[1]);
+                    index2 = Integer.parseInt(action[2]);
+                    numbers[index1] = numbers[index1] * numbers[index2];
+                    break;
+                case "decrease":
+                    for (int i = 0; i < numbers.length; i++) {
+                        numbers[i]=numbers[i]-1;
+                    }
+                    break;
             }
         }
-
-
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i]);
+            if (i < numbers.length - 1) {
+                System.out.print(", ");
+            }
+        }
     }
 }
